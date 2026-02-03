12291d8748d6703db4df3baa9bd1c3a5ec1ab44d
TuTaMyanmar2025 <227531515+tutamyanmar@users.noreply.github.com>
2026-02-03 13:10:13 +0630

    firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+// js/firebase-config.js
+
+// Firebase configuration (သင့်ရဲ့ configuration ကို ဒီမှာထည့်)
+const firebaseConfig = {
+    apiKey: "AIzaSyA82shPz2ClPckCkmnX5FNeiIIaLvJYTO4",
+    authDomain: "lawkesutapanyar.firebaseapp.com",
+    projectId: "lawkesutapanyar",
+    storageBucket: "lawkesutapanyar.firebasestorage.app",
+    messagingSenderId: "963920186949",
+    appId: "1:963920186949:web:a00dabec814b3cda3e1d2c",
+    measurementId: "G-L6B43MJY6S"
+};
+
+export default firebaseConfig;
